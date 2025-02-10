72803e2d65cd869990138462db39bffb90073e3d
HIBBAN QURESHI <qureshihibban@gmail.com>
2025-02-11 03:44:18 +0500

    added

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,12 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     domains: ["images.pexels.com",
       "www.pexels.com"
     ], // Corrected: domains is an array of strings
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
